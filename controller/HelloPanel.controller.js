e4f1f87edcd8d28830be1a5fa849ee2dcd7c971c
Chetanya Sharma <chetanya.s11@gmail.com>
2015-07-21 13:33:36 +0100

    Step 16

--- controller/HelloPanel.controller.js
+++ controller/HelloPanel.controller.js
@@ -1,16 +1,24 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast"
-], function (Controller, MessageToast) {
-   "use strict";
-   return Controller.extend("sap.chet.openUI5.controller.HelloPanel", {
-      onShowHello : function () {
-         // read msg from i18n model
-         var oBundle = this.getView().getModel("i18n").getResourceBundle();
-         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-         // show message
-         MessageToast.show(sMsg);
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast"
+], function(Controller, MessageToast) {
+  "use strict";
+  return Controller.extend("sap.chet.openUI5.controller.HelloPanel", {
+    onShowHello: function() {
+      var oBundle = this.getView().getModel("i18n").getResourceBundle();
+      var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+      var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+      MessageToast.show(sMsg);
+    },
+    _getDialog: function() {
+      if (!this._oDialog) {
+        this._oDialog = sap.ui.xmlfragment("sap.chet.openUI5.view.HelloDialog");
+        this.getView().addDependent(this._oDialog);
       }
-   });
+      return this._oDialog;
+    },
+    onOpenDialog: function() {
+      this._getDialog().open();
+    }
+  });
 });
