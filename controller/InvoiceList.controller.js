6a6117bf99780b67a539a8ca00e322939f6c82fe
Chetanya Sharma <chetanya.s11@gmail.com>
2015-07-23 09:27:17 +0100

    Step 24

--- controller/InvoiceList.controller.js
+++ controller/InvoiceList.controller.js
@@ -1,15 +1,31 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/ui/model/json/JSONModel"
-], function(Controller, JSONModel) {
+  "sap/ui/model/json/JSONModel",
+  "sap/chet/openUI5/model/formatter",
+  "sap/ui/model/Filter",
+  "sap/ui/model/FilterOperator"
+], function(Controller, JSONModel, formatter, Filter, FilterOperator) {
   "use strict";
-
   return Controller.extend("sap.chet.openUI5.controller.InvoiceList", {
+    formatter: formatter,
     onInit: function() {
       var oViewModel = new JSONModel({
         currency: "EUR"
       });
       this.getView().setModel(oViewModel, "view");
+    },
+
+    onFilterInvoices: function(oEvent) {
+      var afilter = [];
+      var sQuery = this.getView().byId("invoiceList");
+      if(sQuery){
+          aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+      }
+
+      var oList = this.getView().byId("invoiceList");
+      var oBinding = oList.getBinding("items");
+      oBinding.filter(aFilter);
     }
+
   });
 });
